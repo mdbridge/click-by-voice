e96f049088f571f487fb705d8f1bc6a3f7f4d0c4
Mark Lillibridge <mdl@alum.mit.edu>
2016-07-06 12:54:17 -0700

    created enumerator for things to hint; merged add and refresh hints functions

--- content_script.js
+++ content_script.js
@@ -1,30 +1,36 @@
+//
+// Labeling elements with hint tags
+//
+
 var next_CBV_hint = 0;
+
+// Enumerate each element that we should hint, possibly more than once:
+function each_hintable(callback) {
+    $("a[href]").each(function(index) {
+	callback($(this));
+    });
+}
+
 
 function add_hints() {
     console.log("adding hints");
 
-    $("a[href]").each(function(index) {
-	$(this).attr("CBV_hint_number", next_CBV_hint);
-	$(this).after("<span CBV_hint_tag='" + next_CBV_hint + "'></span>");
-	next_CBV_hint = next_CBV_hint + 1;
-    });
-}
-
-function refresh_hints() {
-    console.log("refreshing hints");
-
-    $("a[href]").each(function(index) {
-	if ($(this).is("[CBV_hint_number]"))
-	    return;
-
-	$(this).attr("CBV_hint_number", next_CBV_hint);
-	$(this).after("<span CBV_hint_tag='" + next_CBV_hint + "'></span>");
-	next_CBV_hint = next_CBV_hint + 1;
+    each_hintable(function(element) {
+	if (!element.is("[CBV_hint_number]")) {
+	    element.attr("CBV_hint_number", next_CBV_hint);
+	    element.after("<span CBV_hint_tag='" + next_CBV_hint + "'></span>");
+	    next_CBV_hint = next_CBV_hint + 1;
+	}
     });
 
+    console.log("total hints assigned: " + next_CBV_hint);
 }
 
 
+
+//
+// 
+//
 
 function goto_hint(hint) {
     console.log("goto_hint: " + hint);
@@ -36,6 +42,10 @@ function goto_hint(hint) {
 
 
 
+//
+// 
+//
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
 	goto_hint(request.hint_number);
@@ -43,8 +53,7 @@ chrome.runtime.onMessage.addListener(
 
 $(document).ready(function() {
     add_hints();
-    //setTimeout(function() { refresh_hints(); }, 5000);
+    //setTimeout(function() { add_hints(); }, 5000);
     // This runs even when our tab is in the background:
-    setInterval(refresh_hints, 3000);
+    setInterval(add_hints, 3000);
 });
-
