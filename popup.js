e52be055e2db571c192655cb59bb9efe868bd833
Mark Lillibridge <mdl@alum.mit.edu>
2016-07-06 11:11:48 -0700

    close pop-up window after entering a single hint number

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 	// send hint number to content_script.js for current tab:
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 	    chrome.tabs.sendMessage(tabs[0].id, {hint_number: $("#hint_number").val()});
+	    window.close();
 	});
 
 	return false;
