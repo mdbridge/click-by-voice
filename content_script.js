b85cccf25b8cb58a9c80d787a6d39e96e739a477
Mark Lillibridge <mdl@alum.mit.edu>
2016-07-06 10:33:03 -0700

    added basic tags after anchors with hrefs

--- content_script.js
+++ content_script.js
@@ -1,5 +1,12 @@
 function add_hints() {
     console.log("adding hints");
+
+    var i = 0;
+    $("a[href]").each(function(index) {
+	$(this).attr("CBV_hint_number", i);
+	$(this).after("<span CBV_hint_tag='" + i + "'></span>");
+	i = i + 1;
+    });
 }
 
 
