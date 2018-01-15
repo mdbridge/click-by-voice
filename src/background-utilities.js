cc61fc8cc4e24eb876499cea57b4c9800efbbfb7
Mark Lillibridge <mdl@alum.mit.edu>
2018-01-15 10:52:28 -0800

    update comment on when closing the pop-up window is okay

--- src/background-utilities.js
+++ src/background-utilities.js
@@ -21,8 +21,8 @@ function doUserCommand(command_text, close_window) {
 				{hint_number: hint_number,
 				 operation:   operation});
 	if (close_window) {
-	    // have to close pop-up window here; doing so after return
-	    // from doUserCommand breaks sendMessage for some reason
+	    // closing the pop up window ends its JavaScript execution
+	    // so need to do it only after all done here
 	    window.close();
 	}
     });
