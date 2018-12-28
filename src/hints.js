79c169cae29cf0b396645dc82d45a3da14dbdb0e
Mark Lillibridge <mdl@alum.mit.edu>
2018-12-28 08:29:10 -0800

    ignore space as an option

--- src/hints.js
+++ src/hints.js
@@ -117,6 +117,9 @@ var Hints = null;
     }
 
     function parse_option(text) {
+	if (m = text.match(/^\s+(.*)/)) {
+	    text = m[1];
+	}
 	if (m = text.match(/^([^{])\{([^{}]*)\}\{([^{}]*)\}(.*)/)) {
 	    return [m[1], [m[2],m[3]], m[4]];
 	}
