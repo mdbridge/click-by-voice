1c860275a819695fe8c311bd465cec2e67cdc5f8
Mark Lillibridge <mdl@alum.mit.edu>
2020-09-13 11:42:08 -0700

    add refresh, refresh_after_activate options to better control refresh speed

--- src/content_script.js
+++ src/content_script.js
@@ -2,6 +2,22 @@
 /// Main routine
 ///
 
+var next_major_refresh = 0;
+function major_happened() {
+    const major_delay = parseInt(Hints.option_value("refresh", 3000));
+    next_major_refresh = new Date().getTime() + major_delay;
+}
+
+// This runs even when our tab is in the background:
+function maybe_refresh() {
+    const now = new Date().getTime();
+    if (now > next_major_refresh) {
+	Hints.refresh_hints();
+	major_happened();
+    }
+}
+
+
 function perform_operation(operation, hint_number) {
     if (operation == "blur") {
 	document.activeElement.blur();
@@ -20,12 +36,15 @@ function perform_operation(operation, hint_number) {
 	act("set_initial_operation", {initial_operation: operation});
 	Hints.remove_hints();
 	Hints.add_hints(operation.substr(1));
+	major_happened();
     } else if (operation.startsWith("once=")) {
 	Hints.remove_hints();
 	Hints.add_hints(operation.substr(5));
+	major_happened();
     } else {
 	Activate.goto_hint(hint_number, operation);
-	setTimeout(Hints.refresh_hints, 750);
+	next_major_refresh = new Date().getTime() + 
+	    parseInt(Hints.option_value("refresh_after_activate", 500));
     }
 }
 
@@ -37,17 +56,16 @@ if (window == window.top) {
 	    perform_operation(request.operation, request.hint_number);
 	});
 
-
     $(document).ready(function() {
 	request("get_initial_operation", {}, function(response) {
 	    Hints.set_config(response.config);
 	    perform_operation(response.initial_operation, "");
 	});
 
-
-	//setTimeout(function() { add_hints(); }, 5000);
-	// This runs even when our tab is in the background:
-	setInterval(Hints.refresh_hints, 3000);
+	// try and let initial operation above do 1st hint placement,
+	// but fall back on defaults if no response in five seconds:
+	next_major_refresh = new Date().getTime() + 5000;	
+	setInterval(maybe_refresh, 100);
     });
 
 }
