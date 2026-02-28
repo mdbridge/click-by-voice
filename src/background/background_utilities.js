ba5091608c099eb2e9ce749b95854c8b2ef19c18
Mark Lillibridge <mdl@alum.mit.edu>
2026-02-28 14:25:06 -0800

    bug fix for last change

--- src/background/background_utilities.js
+++ src/background/background_utilities.js
@@ -90,9 +90,9 @@ async function do_activate_hint(tab_id, hint_descriptor, operation) {
     // tab_id of -1 means use current tab:
     if (tab_id < 0) {
         const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
-        tab_id = tabs[0].id;
         if (tabs.length === 0)
             return;
+        tab_id = tabs[0].id;
     }
 
     // Determine which frame to send to.
